b3177131c461a0d431addadb52dde36591e33003
katsuya <k_nakano@yumemi.co.jp>
2022-06-08 23:31:40 +0900

    結果を1行で出力するように修正

--- src/dist/index.js
+++ src/dist/index.js
@@ -1,16 +1,18 @@
 "use strict";
 const numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15];
+var result = "";
 for (const num of numbers) {
     if (num % 15 === 0) {
-        console.log("FizzBuzz");
+        result += "FizzBuzz ";
     }
     else if (num % 5 === 0) {
-        console.log("Buzz");
+        result += "Buzz ";
     }
     else if (num % 3 === 0) {
-        console.log("Fizz");
+        result += "Fizz ";
     }
     else {
-        console.log(num);
+        result += `${num} `;
     }
 }
+console.log(result);
--- src/fizz_buzz/index.ts
+++ src/fizz_buzz/index.ts
@@ -1,13 +1,16 @@
 const numbers: number[] = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15];
+var result: string = "";
 
 for (const num of numbers) {
     if (num % 15 === 0) {
-        console.log("FizzBuzz");
+        result += "FizzBuzz ";
     } else if (num % 5 === 0) {
-        console.log("Buzz");
+        result += "Buzz ";
     } else if (num % 3 === 0) {
-        console.log("Fizz");
+        result += "Fizz ";
     } else {
-        console.log(num)
+        result += `${num} `;
     }
 }
+
+console.log(result);
