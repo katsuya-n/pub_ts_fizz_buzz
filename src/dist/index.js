60604d17b5eeea9cd81ea31d499a9311ce3b7fe1
katsuya <k_nakano@yumemi.co.jp>
2022-06-08 23:32:39 +0900

    リファクタリング

--- src/dist/index.js
+++ src/dist/index.js
@@ -2,17 +2,18 @@
 const numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15];
 var result = "";
 for (const num of numbers) {
+    result += " ";
     if (num % 15 === 0) {
-        result += "FizzBuzz ";
+        result += "FizzBuzz";
     }
     else if (num % 5 === 0) {
-        result += "Buzz ";
+        result += "Buzz";
     }
     else if (num % 3 === 0) {
-        result += "Fizz ";
+        result += "Fizz";
     }
     else {
-        result += `${num} `;
+        result += `${num}`;
     }
 }
 console.log(result);
--- src/fizz_buzz/index.ts
+++ src/fizz_buzz/index.ts
@@ -2,14 +2,15 @@ const numbers: number[] = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15];
 var result: string = "";
 
 for (const num of numbers) {
+    result += " ";
     if (num % 15 === 0) {
-        result += "FizzBuzz ";
+        result += "FizzBuzz";
     } else if (num % 5 === 0) {
-        result += "Buzz ";
+        result += "Buzz";
     } else if (num % 3 === 0) {
-        result += "Fizz ";
+        result += "Fizz";
     } else {
-        result += `${num} `;
+        result += `${num}`;
     }
 }
 
